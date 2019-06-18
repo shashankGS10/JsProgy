dbf812a599de3d4282ae559ab0b83185fd3147cc
Shashank G S <49735860+shashankGS10@users.noreply.github.com>
2019-06-18 17:33:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+var bodyParser = require("body-parser");
+const users = require('./data/users.json');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+app.get('/users/:id/about-me', (req, res) => {
+    const result = users.filter(user => user.id === req.params.id)[0];
+    res.send(result.about);
+});
+app.post('/newpost',function(req,res){
+    var sender= req.body.sender_id;
+    console.log(sender);
+    res.send("posted");
+  });
+app.get('/users/:id/feed',(req,res)=>{
+  const result = users.filter(user=>user.id === req.params.id)[0];
+  res.send(result.feed);
+})
+
+app.listen(8080, () => {
+  console.log(`Server listening on port 8080...`);
+});
